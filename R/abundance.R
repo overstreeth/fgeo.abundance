d3815702c63ec1dc65484a6d1d29b2ae25e52ca8
maurolepore <maurolepore@gmail.com>
2017-08-11 19:04:05 -0400

    Move abundanceperquad to R/abundance.R

--- R/abundance.R
+++ R/abundance.R
@@ -1,8 +1,5 @@
 # ***spatial; quadfunc*** -------------------------------------------------
 
-
-# `to_id_*()` -------------------------------------------------------------
-
 #' Convert to quadrat indices.
 #' 
 #' These functions output quadrat indices from either row and column numbers
@@ -68,10 +65,6 @@ to_id_gxgy <- function(gx, gy, gridsize, plotdim) {
   return(to_id_rowcol(.row_adjusted, .col_adjusted, gridsize, plotdim))
 }
 
-
-
-# `to_id_*()`, validate arguments -----------------------------------------
-
 validate_gridsize_plotdim <- function(gridsize, plotdim) {
   assertive::assert_all_are_not_na(gridsize)
   assertive::assert_all_are_not_na(plotdim)
@@ -114,7 +107,63 @@ warn_bad_arg_to_id_rowcol <- function(.row, .col, gridsize, plotdim) {
 
 
 
-# Basal area --------------------------------------------------------------
+# ***utilities; utilities*** ----------------------------------------------
+
+#' Fill 2 dimensional data with custom value.
+#'
+#' This function fills out an array of 2 dimensions with custom values for extra
+#' columns and rows as named in `class1` and `class2`. If a column or row is
+#' missing, it will be filled with the value given by fill. It is useful for
+#' results of table or tapply when some elements had no records.
+#' 
+#' @param .data A two-dimension array, matrix or data.frame.
+#' @param class1,class2 A vector of the same length  of `.data` or longer.
+#' @param fill A numeric value to fill missing values with.
+#'
+#' @aliases fill.dimension
+#' 
+#' @export
+#' @examples 
+#' class1 <- 1:2
+#' class2 <- letters[1:5]
+#' fill = 0
+#' .data <- array(
+#'   data = c(1, 2, NA),
+#'   dim = c(2, 4),
+#'   dimnames = list(1:2, letters[1:4])
+#' )
+fill_dimension <- function(.data, class1, class2, fill = 0) {
+  validate_data_class1_class2_fill(.data, class1, class2, fill)
+  
+  result <- data.frame(
+    matrix(fill, nrow = length(class1), ncol = length(class2))
+  )
+  rownames(result) <- class1
+  colnames(result) <- class2
+
+  result[rownames(.data), colnames(.data)] <- .data
+  result[is.na(result)] <- fill
+  result
+}
+
+validate_data_class1_class2_fill <- function(.data, class1, class2, fill) {
+  # let class1 = NULL pass this test
+  if (!is.null(class1)) {
+    assertive::assert_any_are_not_na(class1, severity = "warning")
+  }
+  assertive::assert_is_of_length(dim(.data), 2, severity = "warning")
+  assertive::assert_any_are_matching_regex(
+    class(.data), "matrix|data.frame", severity = "warning"
+  )
+  class2_is_not_shorter_than_n_cols_of_data <- length(class2) >= ncol(.data)
+  assertive::assert_is_identical_to_true(
+    class2_is_not_shorter_than_n_cols_of_data, severity = "stop"
+  )
+}
+
+
+
+# ***abundance; abundance*** ----------------------------------------------
 
 #' Basal area of trees.
 #' 
@@ -179,10 +228,6 @@ ba_sum <- function(dbh, dbhunit = "mm", mindbh = 10) {
   sum(ba(dbh, dbhunit = dbhunit), na.rm = TRUE)
 }
 
-
-
-# Basal area, validate arguments ------------------------------------------
-
 validate_dbh_dbhunit_mindbh <- function(dbh, dbhunit, mindbh = NULL) {
   assertive::assert_is_numeric(dbh)
   assertive::assert_any_are_matching_regex(dbhunit, pattern = "^mm$|^cm$")
@@ -197,61 +242,210 @@ validate_dbh_dbhunit_mindbh <- function(dbh, dbhunit, mindbh = NULL) {
 
 
 
-# ***utilities; utilities*** ----------------------------------------------
 
-#' Fill 2 dimensional data with custom value.
-#'
-#' This function fills out an array of 2 dimensions with custom values for extra
-#' columns and rows as named in `class1` and `class2`. If a column or row is
-#' missing, it will be filled with the value given by fill. It is useful for
-#' results of table or tapply when some elements had no records.
+
+
+
+
+
+
+
+
+
+
+
+#' Abundance or basal area dividing data with 1 or 2 categorical variables.
 #' 
-#' @param .data A two-dimension array, matrix or data.frame.
-#' @param class1,class2 A vector of the same length  of `.data` or longer.
-#' @param fill A numeric value to fill missing values with.
-#'
-#' @aliases fill.dimension
+#' Calculates total abundance or basal area, dividing data with 1 or 2 
+#' categorical variables passed to `split1` and `split2`.
 #' 
+#' The length of `split1` and `split2` must match exactly the number of rows in
+#' `censdata` (so one per tree or stem). `split1` should be the one with the
+#' most categories (for instances, `split1 = species`, `split2 = dbhcategory`).
+#' 
+#' For basal area, pass a stem table to `censdata`; For abundance, use either
+#' the stem or full table to count stems or trees, respectively.
+#' 
+#' @template mindbh
+#' @template censdata
+#' @template type
+#' @template alivecode
+#' @template dbhunit
+#' @param split1,split2 A vector of categories, one per individual. See details.
+#'
+#' @return A named list of two elements: one is either `abund` or `ba`, and the
+#'   other one is `meandate` (mean measurement date for all individuals in each
+#'   category). Each element of the list is an array of one or two dimensions,
+#'   depending on how many split variables were submitted: each of the
+#'   dimensions of the array handles one of the categorical variables.
+#'
 #' @export
-#' @examples 
-#' class1 <- 1:2
-#' class2 <- letters[1:5]
-#' fill = 0
-#' .data <- array(
-#'   data = c(1, 2, NA),
-#'   dim = c(2, 4),
-#'   dimnames = list(1:2, letters[1:4])
+#' @seealso [ba], [ba_sum].
+#' @template author_condit
+#'
+#' @examples
+#' (tree_abundance <- abundance(bci::bci12full5, mindbh = 10))
+#' (stem_abundance <- abundance(bci::bci12stem5, mindbh = 10))
+#' 
+#' stem_basal_area_by_sp <- abundance(
+#'   bci::bci12stem5,
+#'   type = "ba",
+#'   mindbh = 10,
+#'   split1 = bci::bci12stem5$sp
 #' )
-fill_dimension <- function(.data, class1, class2, fill = 0) {
-  validate_data_class1_class2_fill(.data, class1, class2, fill)
-  
-  result <- data.frame(
-    matrix(fill, nrow = length(class1), ncol = length(class2))
-  )
-  rownames(result) <- class1
-  colnames(result) <- class2
+#' lapply(stem_basal_area_by_sp, head)
 
-  result[rownames(.data), colnames(.data)] <- .data
-  result[is.na(result)] <- fill
+
+
+abundance <- function(censdata,
+                      type = "abund",
+                      alivecode = c("A"),
+                      mindbh = NULL,
+                      dbhunit = "mm",
+                      split1 = NULL,
+                      split2 = NULL) {
+  if (is.null(split1)) {
+    split1 <- rep("all", dim(censdata)[1])
+  }
+  if (is.null(split2)) {
+    split2 <- rep("all", dim(censdata)[1])
+  }
+
+  if (!is.null(mindbh)) {
+    inc <- censdata$dbh >= mindbh
+  } else {
+    inc <- rep(TRUE, length(censdata$dbh))
+  }
+ 
+  alive <- rep(FALSE, length(censdata$dbh))
+  for (i in 1:length(alivecode)) {
+    alive[censdata$status == alivecode[i]] <- TRUE
+  }
+
+  class1 <- sort(unique(split1))
+  class2 <- sort(unique(split2))
+  groupvar <- list(split1[alive & inc], split2[alive & inc])
+  
+  stopifnot(type %in% c("abund", "ba", "agb"))
+  if (type == "abund") {
+    abund <- tapply(censdata$dbh[alive & inc], groupvar, length)
+  }
+  if (type == "ba") {
+    abund <- tapply(
+      censdata$dbh[alive & inc],
+      groupvar,
+      ba_sum, mindbh = mindbh, dbhunit = dbhunit
+    )
+  } 
+  if (type == "agb") {
+    abund <- tapply(
+      censdata$agb[alive & inc], 
+      groupvar, 
+      sum, na.rm = TRUE
+    )
+  }
+
+  meandate <- tapply(censdata$date[alive & inc], groupvar, mean, na.rm = TRUE)
+  
+  abund_filled <- fill_dimension(abund, class1, class2)
+  meandate_filled <- fill_dimension(meandate, class1, class2, fill = NA)
+  
+  result <- list(abund = abund_filled, meandate = meandate_filled)
+  if (type == "ba") {
+    names(result)[1] = "ba"
+  }
+  if (type == "agb") {
+    names(result)[1] = "agb"
+  }
   result
 }
 
 
 
-# utilities, validate arguments -------------------------------------------
 
-validate_data_class1_class2_fill <- function(.data, class1, class2, fill) {
-  # let class1 = NULL pass this test
-  if (!is.null(class1)) {
-    assertive::assert_any_are_not_na(class1, severity = "warning")
-  }
-  assertive::assert_is_of_length(dim(.data), 2, severity = "warning")
-  assertive::assert_any_are_matching_regex(
-    class(.data), "matrix|data.frame", severity = "warning"
-  )
-  class2_is_not_shorter_than_n_cols_of_data <- length(class2) >= ncol(.data)
-  assertive::assert_is_identical_to_true(
-    class2_is_not_shorter_than_n_cols_of_data, severity = "stop"
-  )
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' 
+#' #' Abundance, basal area, or agb of every species by quadrat.
+#' #'
+#' #' @description
+#' #' Finds abundance, basal area, or agb of every species per square quadrat of
+#' #' any size; plotdim is the x dimension then y dimension of the plot and must be
+#' #' set correctly; gridsize is the quadrat dimension. The plot is divided into a
+#' #' checkerboard of non-overlapping, space-filling squares.
+#' #'
+#' #' @details
+#' #' If the plot dimensions is not an exact multiple of the quadrat size, then a 
+#' #' strip at the upper edge of the plot (north and east if plot is on cardinal 
+#' #' directions) is omitted. For example, if `gridsize = 40` and `plotdim = 500`,
+#' #' then there are an extra 20 meters at the upper boundary omitted from the 
+#' #' calculations.
+#' #'
+#' #' The array of abundances per quadrat is useful for similarity, counting
+#' #' species and stems per quadrat, etc.
+#' #' 
+#' #' @inheritParams abundance
+#' #' @inheritParams findborderquads
+#' #' 
+#' #' @seealso [abundance()]
+#' #' 
+#' #' @return See [abundance()].
+#' #'
+#' #' @examples
+#' #' \dontrun{
+#' #' Nperquad = abundanceperquad(
+#' #'   bci::bci12full6,
+#' #'   plotdim = c(1000, 500),
+#' #'   gridsize = 100,
+#' #'   type = 'abund'
+#' #' )
+#' #' colSums(Nperquad$abund)
+#' #' apply(Nperquad$abund, 2, countspp)
+#' #' plot(colSums(Nperquad$abund), apply / (Nperquad$abund, 2, countspp))
+#' #' }
+#' abundanceperquad=function(censdata,mindbh=10,plotdim=c(1000,500),gridsize=100,type='abund',dbhunit='mm')
+#' {
+#'  sp=censdata$sp
+#' 
+#'  quadno=gxgy.to.index(censdata$gx,censdata$gy,gridsize=gridsize,plotdim=plotdim)
+#'  result=abundance(censdata,type=type,mindbh=mindbh,dbhunit=dbhunit,split1=sp,split2=quadno)
+#' 
+#'  allspp=unique(censdata$sp)
+#'  maxquad=floor(plotdim[1]/gridsize)*floor(plotdim[2]/gridsize)
+#'  allquad=1:maxquad
+#' 
+#'  if(dim(result[[type]])[1]<length(allspp) | dim(result[[type]])[2]<length(allquad))
+#'      result[[type]]=fill.dimension(result[[type]],class1=allspp,class2=allquad,fill=0)
+#'   
+#'  return(result)
+#' }
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ tests/testthat/test-abundance.R
@@ -0,0 +1,28 @@
+context("abundance")
+
+library(dplyr)
+cns5_mini <- sample_n(bci::bci12full5, 100)
+
+test_that("outputs the same as ctfs::abundance()", {
+  old <- ctfs::abundance(cns5_mini)
+  new <- forestr::abundance(cns5_mini)
+  expect_equal(old, new)
+  
+  old <- ctfs::abundance(cns5_mini, mindbh = 10)
+  new <- forestr::abundance(cns5_mini, mindbh = 10)
+  expect_equal(old, new)
+
+  old <- ctfs::abundance(
+    cns5_mini,
+    type = "ba",
+    mindbh = 10,
+    split1 = cns5_mini$sp
+  )
+  new <- forestr::abundance(
+    cns5_mini,
+    type = "ba",
+    mindbh = 10,
+    split1 = cns5_mini$sp
+  )
+  expect_equal(old, new)
+})
