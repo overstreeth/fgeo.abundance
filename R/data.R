78b62edb2ccb818247e2e31e1ccea446703a3083
maurolepore <maurolepore@gmail.com>
2017-08-15 16:37:33 -0400

    Add minimal data documentation.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Minimal data from Barro Colorado Island for examples.
+#' 
+#' @seealso https://forestgeo.github.io/bci
+#' 
+'bci_mini'
+
+#' Habitat data from Barro Colorado Island.
+#' 
+#' @seealso https://forestgeo.github.io/bci/reference/bci_habitat.html
+#' 
+'bci_habitat'
