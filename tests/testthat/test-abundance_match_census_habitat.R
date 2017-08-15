4fcec341e52fe48cb334b92d77f477bd1c3c032b
maurolepore <maurolepore@gmail.com>
2017-08-15 06:35:58 -0400

    Compare abundance_match_census_habitat() to abundanceperquad

--- tests/testthat/test-abundance_match_census_habitat.R
+++ tests/testthat/test-abundance_match_census_habitat.R
@@ -1,10 +1,9 @@
 context("abundance_match_census_habitat")
 
 test_that("value is a data frame with subset equal to stored reference", {
-  value <- abundance_match_census_habitat(bci_mini, bci_habitat)
-  expect_is(value, "data.frame")
-  actual <- value[1:15, 1:15]
-  expect_equal_to_reference(
-    actual,
-    "ref_abundance_match_census_habitat.rds")
+  ref <- abundanceperquad(
+    bci_mini, plotdim = c(1000, 500), gridsize = 20
+  )$abund
+  actual <- abundance_match_census_habitat(bci_mini, bci_habitat)
+  expect_equal(ref, actual)
 })
