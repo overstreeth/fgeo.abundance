c2338e7086b6caf121fa576aa8bf7e8fd7f5442e
maurolepore <maurolepore@gmail.com>
2017-08-29 09:01:09 -0400

    Repair test error by adding bciex:: to its data.

--- tests/testthat/test-count_alive_by.R
+++ tests/testthat/test-count_alive_by.R
@@ -1,13 +1,7 @@
-# Filters alive and adds unique id
-id_alive <- function(x) {
-  alive <- x[x$status == "A", ]
-  alive$id <- paste0(alive$treeID, "_", alive$stemID)
-  alive
-}
-
 context("id_alive")
+
 test_that("has the extra variable id", {
-  x <- bci12t7mini
+  x <- bciex::bci12t7mini
   expect_equal(length(id_alive(x)), length(x) + 1)
   
   expect_true(
@@ -19,39 +13,25 @@ test_that("has the extra variable id", {
 
 
 
-count_alive <- function(x) {
-  alive_id <- id_alive(x)
-  length(unique(alive_id$id))
-}
-
-library(testthat)
 context("count_alive")
 
 test_that("outputs the same as ctfs::abundance when type = 'abund'.", {
   # Tree data
-  now <- count_alive(bci12t1mini)
-  before <- unname(ctfs::abundance(bci12t1mini)$abund$all)
+  now <- count_alive(bciex::bci12t1mini)
+  before <- unname(ctfs::abundance(bciex::bci12t1mini)$abund$all)
   expect_equal(before, now)
   # Stem data
-  now <- count_alive(bci12s1mini)
-  before <- unname(ctfs::abundance(bci12s1mini)$abund$all)
+  now <- count_alive(bciex::bci12s1mini)
+  before <- unname(ctfs::abundance(bciex::bci12s1mini)$abund$all)
   expect_equal(before, now)
 })
 
 
 
-count_alive_by <- function(x, ...) {
-  all_sp <- unique(x["sp"])
-  alive <- id_alive(x)
-  count_alive <- dplyr::count(alive, ...)
-  count_all_sp <- dplyr::right_join(count_alive, all_sp)
-  tidyr::replace_na(count_all_sp, list(n = 0))
-}
-
 context("count_alive_by")
 
 test_that("outputs the same as ctfs::abundance with defaults", {
-  x <- bci12t7mini
+  x <- bciex::bci12t7mini
   now <- count_alive_by(x, sp) %>% arrange(sp)
   before <- x %>%
     split(.$sp) %>%
@@ -63,11 +43,3 @@ test_that("outputs the same as ctfs::abundance with defaults", {
     arrange(sp)
   expect_equal(now, before)
 })
-
-
-
-
-
-
-
-
