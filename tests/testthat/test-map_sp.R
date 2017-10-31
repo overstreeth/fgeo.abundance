3b223116c57d58508b57646e2e6d61d5b364c0b5
maurolepore <maurolepore@gmail.com>
2017-10-31 16:55:06 -0400

    rm empty test

--- tests/testthat/test-map_sp.R
+++ tests/testthat/test-map_sp.R
@@ -48,11 +48,3 @@ test_that("output is an invisible and named list and prints pdf w/ message", {
   expect_named(result, all_species)
 })
 
-
-
-context("map_xy")
-
-test_that("plots elevation or not based on argument elev.", {
-  # xxx
-})
-
