d139ae6e19ec224075597f5ed8e73725212a3bc2
maurolepore <maurolepore@gmail.com>
2017-11-01 16:29:30 -0400

    Start map vignette.

--- tests/testthat/test-map_sp.R
+++ tests/testthat/test-map_sp.R
@@ -1,12 +1,4 @@
-context("map_sp")
-
-# # Minimal data to show
-# census <- bci::bci12full7
-# # Filtering only 3 species for a minimal example.
-# (species_selected <- unique(census$sp)[1:3])
-# census_sub <- dplyr::filter(census, sp %in% species_selected)
-# # Sampling only 1000 rows for a quick example
-# census_sub <- dplyr::sample_n(census_sub, 1000)
+# setup -------------------------------------------------------------------
 
 library(dplyr)
 
@@ -20,21 +12,20 @@ census <- census %>% dplyr::filter(sp  %in% few_sp)
 # Selecting all species in the example dataset
 all_species <- unique(census$sp)
 
-
+context("map_sp")
 
 test_that("errs with wrong inputs.", {
+  # wrong name
+  expect_error(
+    map_sp(census = dplyr::rename(census, spp = sp), 
+    species = all_species)
+  )
   # wrong species
   expect_error(map_sp(census = census, species = 1:3))
-  # wrong name
-  expect_error(map_sp(
-    census = dplyr::rename(census, spp = sp),
-    species = all_species
-    )
-  )
 })
 
 test_that("outputs a non empty list", {
-  result <-  map_sp(species = all_species, census = census)
+  result <-  map_sp(census = census, species = all_species)
   expect_type(result, "list")
   expect_silent(
     lapply(result, assertive::is_non_empty) %>%
@@ -55,5 +46,13 @@ test_that("output is an invisible and named list and prints pdf w/ message", {
   expect_type(result, "list")
   expect_length(result, length(all_species))
   expect_named(result, all_species)
+  # Warns that given name is replaced by default
+  expect_warning(
+    # Message informs name used is default
+    expect_message(
+      map_sp_pdf(census = census, species = all_species, file = "bad"),
+      "Saving as map.pdf"
+    )
+  )
 })
 
--- tmp.R
+++ tmp.R
@@ -6,19 +6,37 @@ census <- census %>% filter(sp  %in% few_sp)
 
 elevation <- bciex::bci_elevation
 elevation <- dplyr::rename(elevation, gx = x, gy = y)
+
+under250 <- census %>% filter(gx <250, gy < 250) 
+map_sp(under250, "hybapr", xlim = c(0, 800), ylim = c(0, 900))
+map_sp(census, "hybapr")
+
 xlimit <- c(0, max(census$gx, na.rm = TRUE))
 ylimit <- c(0, max(census$gy, na.rm = TRUE))
 
-map_xy(census, xlimit, ylimit)
-map_one_sp(census, "hybapr", elevation = elevation, bins = NULL)
 
 
 
+p <- map_basic(census, xlimit, ylimit)
+
+myelev <- elevation
+add_elevation(p, myelev, line_size = .75)
+
+map_one_sp(census = census, one_sp = "hybapr")
 
 
-map_sp(census, c("hybapr", "faraoc"), elevation = elevation, bins = 20)
+map_one_sp(census, "hybapr", elevation = elevation, low = "red", high = "black",
+  theme = ggplot2::theme(text = element_text(size = 20)), line_size = .2)
+
+
+map_sp(census, c("hybapr", "faraoc"), theme = ggplot2::theme_dark(),
+  elevation = elevation)
+
+check_map_sp(census, "hybapr")
+
 
 species <- c("hybapr", "faraoc")
-map_sp_pdf(census, species, file = "mymap.pdf", elevation = elevation, bins = 2)
+map_sp_pdf(census, species, file = "mymap.pdf", theme = ggplot2::theme_minimal(),
+  elevation = elevation, bins = 2, line_size = 2)
 
 
