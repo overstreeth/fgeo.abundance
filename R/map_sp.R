316f90de9ed275073c0645bd84d8454420e829fa
maurolepore <maurolepore@gmail.com>
2017-11-01 15:46:58 -0400

    Move ... to the end of map_*() functions to let R complain if arguments are wrongly matched.

--- R/map_sp.R
+++ R/map_sp.R
@@ -13,9 +13,6 @@
 #'   `xlim = c(0, 1000), ylim = c(0, 500)`. Default limits should be OK -- they
 #'   are set to be c(0, max), where max is the maximum value of `gx` or `gy`
 #'   in the data set.
-#' @param ... Arguments passed to [ggplot2::geom_layer()] via
-#'   [ggplot2::geom_point()] to customize, for example, the size, shape,
-#'   or colour of the points.
 #' @param theme A ggplot2 theme to customize the looks of the map.
 #' @param elevation A dataframe with variables gx, gy, and elev giving the 
 #'   elevation of the site.
@@ -27,6 +24,9 @@
 #' @param bins A number. Setting bins creates evenly spaced contours in the
 #'   range of the data. Integers 
 #' @param file A character string giving the name of the file.
+#' @param ... Arguments passed to [ggplot2::geom_layer()] via
+#'   [ggplot2::geom_point()] to customize, for example, the size, shape,
+#'   or colour of the points.
 #'
 #' @seealso [ggplot2::geom_layer()], [grDevices::pdf()], [grDevices::png()], 
 #'   [ggplot2::theme()].
@@ -97,13 +97,13 @@ map_sp <- function(census,
                    species,
                    xlim = NULL,
                    ylim = NULL,
-                   ...,
                    theme = ggplot2::theme_bw(),
                    elevation = NULL,
                    line_size = 0.5,
                    low = "#132B43",
                    high = "#56B1F7",
-                   bins = NULL) {
+                   bins = NULL,
+                   ...) {
   check_map_sp(census, species)
   
   plots <- lapply(X = species, FUN = map_one_sp, census = census, 
@@ -120,14 +120,14 @@ map_sp_pdf <- function(census,
                        species,
                        xlim = NULL,
                        ylim = NULL,
-                       ...,
                        theme = ggplot2::theme_bw(),
                        elevation = NULL,
                        line_size = 0.5,
                        low = "#132B43",
                        high = "#56B1F7",
                        bins = NULL,
-                       file = "map.pdf") {
+                       file = "map.pdf",
+                       ...) {
   check_map_sp(census, species)
   file <- check_file_extension(file)
   
@@ -140,6 +140,33 @@ map_sp_pdf <- function(census,
   invisible(lapply(plots, print))
   message("Saving as ", file)
   invisible(plots)
+}
+
+# Standarized plot for each species (fixed ratio and limits).
+map_one_sp <- function(census,
+                       one_sp,
+                       xlim = NULL,
+                       ylim = NULL,
+                       theme = ggplot2::theme_bw(),
+                       elevation = NULL,
+                       line_size = 0.5,
+                       low = "#132B43",
+                       high = "#56B1F7",
+                       bins = NULL,
+                       ...) {
+  assertive::assert_is_character(one_sp)
+  assertive::assert_is_of_length(one_sp, 1)
+  
+  if (is.null(xlim)) {xlim <- c(0, max(census$gx, na.rm = TRUE))}
+  if (is.null(ylim)) {ylim <- c(0, max(census$gy, na.rm = TRUE))}
+
+  filtered_census <- census[census$sp %in% one_sp, ]
+  p <- map_basic(filtered_census, xlim, ylim, theme = theme, ...)
+  if (!is.null(elevation)) {
+    p <- add_elevation(ggplot = p, elevation = elevation, line_size = line_size,
+      low = low, high = high, bins = bins)
+  }
+  p
 }
 
 # General plot of gx by gy faceted by species.
@@ -179,31 +206,4 @@ add_elevation <- function(ggplot,
     "enlarge.box", box.color = NA, fill = "transparent", "draw.rects")
   p_with_labels <- directlabels::direct.label(p, labels_properties)
   p_with_labels
-}
-
-# Standarized plot for each species (fixed ratio and limits).
-map_one_sp <- function(census,
-                       one_sp,
-                       ...,
-                       xlim = NULL,
-                       ylim = NULL,
-                       theme = ggplot2::theme_bw(),
-                       elevation = NULL,
-                       line_size = 0.5,
-                       low = "#132B43",
-                       high = "#56B1F7",
-                       bins = NULL) {
-  assertive::assert_is_character(one_sp)
-  assertive::assert_is_of_length(one_sp, 1)
-  
-  if (is.null(xlim)) {xlim <- c(0, max(census$gx, na.rm = TRUE))}
-  if (is.null(ylim)) {ylim <- c(0, max(census$gy, na.rm = TRUE))}
-
-  filtered_census <- census[census$sp %in% one_sp, ]
-  p <- map_basic(filtered_census, xlim, ylim, theme = theme, ...)
-  if (!is.null(elevation)) {
-    p <- add_elevation(ggplot = p, elevation = elevation, line_size = line_size,
-      low = low, high = high, bins = bins)
-  }
-  p
 }
