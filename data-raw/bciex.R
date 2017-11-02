6ccbe7f8cac07888b5fc33a0046cd0a4f452949e
maurolepore <maurolepore@gmail.com>
2017-11-02 18:35:46 -0400

    Add elevation data for examples.

--- data-raw/bciex.R
+++ data-raw/bciex.R
@@ -2,11 +2,11 @@
 
 library(bciex)
 
+usethis::use_data(bci12s6mini)
+usethis::use_data(bci12s7mini)
 
+usethis::use_data(bci12t6mini)
+usethis::use_data(bci12t7mini)
 
-use_data(bci12s6mini)
-use_data(bci12s7mini)
-
-use_data(bci12t6mini)
-use_data(bci12t7mini)
+usethis::use_data(bci_elevation)
 
