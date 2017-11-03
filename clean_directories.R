b4af151439e6a0a075242cbf1d71b9ba13f78c5b
maurolepore <maurolepore@gmail.com>
2017-11-03 12:36:36 -0400

    Update to clean_directories to work on tutorials/ instead of vignettes/

--- clean_directories.R
+++ clean_directories.R
@@ -1,7 +1,8 @@
 bases <- c(
   "./",
-  "./vignettes/",
+  "./inst/tutorials/",
   "./tests/testthat/"
 )
 lapply(bases, rm_files, file = "pdf$")
 lapply(bases, rm_files, file = "png$")
+lapply(bases, rm_files, file = "html$")
