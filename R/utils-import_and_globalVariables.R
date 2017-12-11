99e23255ec34e06acd87f00b9778cb2762426eb9
maurolepore <maurolepore@gmail.com>
2017-12-11 11:35:39 -0500

    Simplify style in listed globalVariables.

--- R/utils-import_and_globalVariables.R
+++ R/utils-import_and_globalVariables.R
@@ -43,13 +43,4 @@ NULL
 # global variables --------------------------------------------------------
 
 # Flag inline helpers as global variables so R CMD check doesn't warn
-utils::globalVariables(
-  c(
-    # rlang
-    ".data",
-    # dplyr
-    "n",
-    # ggplot2
-    "gx", "gy"
-  )
-)
+utils::globalVariables(c(".data", "n", "gx", "gy"))
