8cbce7e0966ac6ae0903fa306d1bac7b57f0b675
maurolepore <maurolepore@gmail.com>
2018-06-12 14:47:24 -0400

    forget cached

--- clean_directories.R
+++
@@ -1,8 +0,0 @@
-bases <- c(
-  "./",
-  "./inst/tutorials/",
-  "./tests/testthat/"
-)
-lapply(bases, rm_files, file = "pdf$")
-lapply(bases, rm_files, file = "png$")
-lapply(bases, rm_files, file = "html$")
