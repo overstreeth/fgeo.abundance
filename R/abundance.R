1547655838de76871a78c5544726ac85c9fac164
maurolepore <maurolepore@gmail.com>
2018-06-25 17:29:51 -0400

    Add count_distinct() and friends.

--- R/abundance.R
+++ R/abundance.R
@@ -1,3 +1,76 @@
+#' Count distinct occurrences of a variable by groups (tree or stem abundance).
+#' 
+#' These functions are usually used on grouped data created by
+#' `dplyr::group_by()` (reexported by __fgeo__). The output will have one row
+#' for each group. 
+#' * `count_distinct()` counts distinct occurrences of a variable.
+#' * `abundance_stem()` and `abundance_tree()` are simpler wrappers for the
+#' specifically counting distinct occurrences of the variables `stemID` and
+#' `treeID` which uniquely identify each stem and tree in census datasets of
+#' ForestGEO (both stem and tree tables).
+#' 
+#' @param .data A dataframe, commonly grouped with `group_by()`.
+#' @param .var A variable to count distinct occurances.
+#' 
+#' @seealso [dplyr::group_by()], [dplyr::summarise()].
+#' 
+#' @return An object of the same class as .data. One grouping level will be
+#'   dropped.
+#' @export
+#'
+#' @examples
+#' # Example data
+#' census <- tibble::tibble(
+#'   treeID = c(1, 1, 2, 3, 3, 3),
+#'   stemID = c(1, 2, 3, 4, 5, 6),
+#'   quadrat = paste0("000", rep(1:2, each = 3)),
+#'   sp = c(paste0("sp", c(1, 1, 2)), paste0("sp", c(3, 3, 3))),
+#'   dbh = abs(sample(rnorm(100), 6) * 10)
+#' )
+#' census
+#' 
+#' count_distinct(census, stemID)
+#' 
+#' by_quad <- group_by(census, quadrat)
+#' count_distinct(by_quad, stemID)
+#' count_distinct(by_quad, treeID)
+#' 
+#' abundance_stem(by_quad)
+#' abundance_tree(by_quad)
+#' 
+#' by_sp <- group_by(census, sp)
+#' abundance_stem(by_sp)
+#' abundance_tree(by_sp)
+#' 
+#' by_quad_sp <- group_by(census, quadrat, sp)
+#' abundance_stem(by_quad_sp)
+#' abundance_tree(by_quad_sp)
+count_distinct <- function(.data, .var) {
+  .var <- enquo(.var)
+  dplyr::summarise(.data, n = dplyr::n_distinct(!! .var))
+}
+
+#' @export
+#' @rdname count_distinct
+abundance_stem <- function(.data) {
+  count_distinct(.data, .data$stemID)
+}
+
+#' @export
+#' @rdname count_distinct
+abundance_tree <- function(.data) {
+  count_distinct(.data, .data$treeID)
+}
+
+
+
+
+
+
+
+
+
+
 #' Count individuals in total or by groups (abundance, richness, singletons).
 #' 
 #' These functions help you to count rows in a dataframe:
--- tests/testthat/test-abundance.R
+++ tests/testthat/test-abundance.R
@@ -1,3 +1,36 @@
+context("count_distinct")
+
+library(dplyr)
+
+census <- tibble::tibble(
+  treeID = c(1, 1, 2, 3, 3, 3),
+  stemID = c(1, 2, 3, 4, 5, 6),
+  quadrat = rep(1:2, each = 3),
+  sp = c(paste0("sp", c(1, 1, 2)), paste0("sp", c(3, 3, 3))),
+  dbh = 1
+)
+census
+
+test_that("counts as expected", {
+  expect_equal(pull(count_distinct(census, stemID), n), 6)
+  
+  by_quad <- group_by(census, quadrat)
+  expect_equal(pull(count_distinct(by_quad, stemID), n), c(3, 3))
+  expect_equal(pull(abundance_stem(by_quad), n), c(3, 3))
+  expect_equal(pull(count_distinct(by_quad, treeID), n), c(2, 1))
+  expect_equal(pull(abundance_tree(by_quad), n), c(2, 1))
+  
+  by_sp <- group_by(census, sp)
+  expect_equal(pull(abundance_stem(by_sp), n), c(2, 1, 3))
+  expect_equal(pull(abundance_tree(by_sp), n), c(1, 1, 1))
+  
+  by_quad_sp <- group_by(census, quadrat, sp)
+  expect_equal(pull(abundance_stem(by_quad_sp), n), c(2, 1, 3))
+  expect_equal(pull(abundance_tree(by_quad_sp), n), c(1, 1, 1))
+})
+
+
+
 context("count_duplicated.R")
 
 test_that("works if grouping by variable called n", {
