5276e58cff1b7b429b2bf42ffb29dc4c013a1c17
maurolepore <maurolepore@gmail.com>
2018-07-14 15:12:30 -0400

    Remove useless.

--- tests/testthat/test-fill_na_with_single_unique.R
+++
@@ -1,7 +0,0 @@
-context("fill_na_with_single_unique")
-
-test_that("outputs is as expected", {
-  expect_error(fill_na_with_single_unique(c(1, 2, NA)), "must be of length 1")
-  expect_equal(fill_na_with_single_unique(c(1, NA)), c(1, 1))
-  expect_equal(fill_na_with_single_unique(c(1, 1, NA)), c(1, 1, 1))
-})
