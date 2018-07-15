8cd65a0c3202990c1273849344cb08f870c36eec
maurolepore <maurolepore@gmail.com>
2018-07-15 11:54:15 -0400

    Move collapse_treeid to fgeo.tool.

--- R/utils.R
+++ R/utils.R
@@ -10,19 +10,5 @@ space <- function(...) {
   paste(unlist(list(...)), collapse = " ")
 }
 
-multiple_var <- function(var) {
-  force(var)
-  var <- tolower(var)
-  function(.data) {
-    .data <- stats::setNames(.data, tolower(names(.data)))
-    .var <- .data[[var]]
-    var %in% names(.data) && length(unique(stats::na.omit(.var))) > 1
-  }
-}
-
-multiple_censusid <- multiple_var("censusid")
-
-multiple_plotname <- multiple_var("plotname")
-
 pull_name <- function(x) gsub(".*\\$", "", x)
 
