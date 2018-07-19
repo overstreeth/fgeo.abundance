e616d3b56f7c83f910cbf8b3353e7c26aa53412b
maurolepore <maurolepore@gmail.com>
2018-07-19 17:22:59 -0400

    Add draft of pick_hom_largest().

---
+++ tmp.R
@@ -0,0 +1,30 @@
+Use internal used in building pick_dhh_largest() to write fun to pick_hom_largest() by census is treeid by stemid. Err if multiple plots are detected.
+
+Use this inside count_woods(), right before pick_dbh_largest().
+
+
+Extract factory pick_var_by_group_by_censusid()
+
+function(var, group) {
+  function(.x, .arrange, .pick) {
+    # code
+  }
+}
+
+pick_hom_by_stemid_by_censusid <-
+  pick_var_by_group_by_censusid(hom, stemid)
+
+
+pick_hom_largest <- function(.x) {
+  pick_hom_by_stemid_by_censusid(
+    .x, 
+    .arrange = dplyr::desc, 
+    .pick = row_number() == 1
+  )
+}
+
+
+
+
+asap
+
